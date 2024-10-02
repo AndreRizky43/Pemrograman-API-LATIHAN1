7d402181a2516348eba8968b3a45434677c69099
AndreRizky43 <134258614+AndreRizky43@users.noreply.github.com>
2024-10-02 23:37:01 +0700

    Add files via upload

---
+++ app/page.tsx
@@ -0,0 +1,14 @@
+import Link from "next/link"
+
+export default function Home() {
+  return (
+    <>
+      <h1>TESTING</h1>
+      <Link href="/posts">POSTINGAN PAGE</Link>
+      <br /> 
+      <Link href="/albums">ALBUM PAGE</Link>
+    </>
+    
+  )
+}
+ 
